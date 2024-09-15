cec3e4b741cb5c7a50f951757ffa5e5d60b58726
Raihan <rehanolimit@gmail.com>
2024-09-15 16:07:09 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Mengaktifkan CORS dengan opsi
+  app.enableCors({
+    origin: 'http://localhost:3000', // Hanya mengizinkan permintaan dari origin ini
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
